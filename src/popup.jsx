eda1df5803f77070f8d1f69824191ace0e2046bf
jvxiao <2805431021@qq.com>
2025-04-16 00:34:29 +0800

    feat: enhance app with react-router-dom

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
+import { ZhihuTrending, ToutiaoTrending, WeiboTrending, DouyinTrending} from "./components/Trending";
+import { HomePage, Navigator } from "./components/Home";
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import './css/home.scss';
+
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <div>
     <h2> 🚀 Hello, Trending Hub ! </h2>
+    <Router>
+      <Navigator />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/zhihu" element={<ZhihuTrending />}></Route>
+        <Route path="/toutiao" element={<ToutiaoTrending />}></Route>
+        <Route path="/douyin" element={<DouyinTrending />}></Route>
+        <Route path="/weibo" element={<WeiboTrending />}></Route>
+      </Routes>
+    </Router>
   </div>
 )
