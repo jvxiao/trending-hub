e14f6509316ce3455d4e8193996969f3886e9f7d
jvxiao <2805431021@qq.com>
2025-04-25 23:12:55 +0800

    refactor: extract trending page into one component

--- src/popup.jsx
+++ src/popup.jsx
@@ -6,21 +6,25 @@ import { One } from './components/One'
 import {BrowserRouter as Router, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom'
 import './css/home.scss';
 
+const App = () => {
+  return (
+    <Router>
+    <One />
+    <Navigator />
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/zhihu" element={<ZhihuTrending />}></Route>
+      <Route path="/toutiao" element={<ToutiaoTrending />}></Route>
+      <Route path="/douyin" element={<DouyinTrending />}></Route>
+      <Route path="/weibo" element={<WeiboTrending />}></Route>
+      <Route path="*" element={<HomePage/>} />
+    </Routes>
+  </Router>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <div>
-    {/* <h2> 🚀 Hello, Trending Hub ! </h2> */}
-    <Router>
-      <One />
-      <Navigator />
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/zhihu" element={<ZhihuTrending />}></Route>
-        <Route path="/toutiao" element={<ToutiaoTrending />}></Route>
-        <Route path="/douyin" element={<DouyinTrending />}></Route>
-        <Route path="/weibo" element={<WeiboTrending />}></Route>
-        <Route path="*" element={<HomePage/>} />
-      </Routes>
-    </Router>
+    <App />
   </div>
 )
