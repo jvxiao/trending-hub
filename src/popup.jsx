03bcdc982a3b1ee45c2b1c1a02b600721b466824
jvxiao <2805431021@qq.com>
2025-04-27 00:40:38 +0800

    feat: add context 'mode'

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,31 +1,35 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import {TrendingMap} from "./components/Trending";
+import ReactDOM from "react-dom/client";
+import { TrendingMap } from "./components/Trending";
 import { HomePage, Navigator } from "./components/Home";
-import { One } from './components/One'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './css/home.scss';
+import { One } from "./components/One";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./css/home.scss";
+import { createContext, useContext } from "react";
+
+const ModeContext = createContext("entertainment");
 
 const App = () => {
+  const [mode, setMode] = useContext("ent");
   return (
-    <Router>
-    <One />
-    <Navigator />
-    <Routes>
-      {
-        Object.entries(TrendingMap).map(item => {
-          const [path, component] = item;
-          return <Route path={path} element={component} />
-        })
-      }
-      <Route path="*" element={<HomePage/>} />
-    </Routes>
-  </Router>
-  )
-}
+    <ModeContext.Provider values={{ mode, setMode }}>
+      <Router>
+        <One />
+        <Navigator />
+        <Routes>
+          {Object.entries(TrendingMap).map((item) => {
+            const [path, component] = item;
+            return <Route path={path} element={component} />;
+          })}
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </ModeContext.Provider>
+  );
+};
 
-ReactDOM.createRoot(document.getElementById('app')).render(
+ReactDOM.createRoot(document.getElementById("app")).render(
   <div>
     <App />
   </div>
-)
+);
